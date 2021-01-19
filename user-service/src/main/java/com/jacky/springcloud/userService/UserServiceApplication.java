853bf49eb66fc0b9d4cce27be5e980b785d079c8
fmp <fmp_higherfly@foxmail.com>
2021-01-19 16:58:43 +0800

    ‘cloud初步’

---
+++ user-service/src/main/java/com/jacky/springcloud/userService/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jacky.springcloud.userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
